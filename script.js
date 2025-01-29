33b7235d9f5dd694b6e516442da8ec6573e0f1da
Fatimoh Bakare <fatimohbakare3@gmail.com>
2025-01-29 13:20:11 +0100

    update file

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-const currentDayOfTheWeek = document.querySelector ('[data-testid="currentDay"]');
 const currentUtcTime = document.querySelector('[data-testid="currentTimeUTC"]');
 
-const daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
 const currentDate =new  Date();
-const day = currentDate.getDay();
-const currentDay = daysOfWeek[day];
 const currentTime = currentDate.toUTCString()
 
-currentDayOfTheWeek.textContent = currentDay;
 currentUtcTime.textContent = currentTime;
